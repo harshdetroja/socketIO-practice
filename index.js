a09ea66cc9823fa2884def05e11c5cb7d3fe0703
Harsh Detroja <harshdetroja2001@gmail.com>
2024-08-06 12:45:16 +0530

    emmiting events done

--- index.js
+++ index.js
@@ -2,14 +2,26 @@ import express from "express";
 import { createServer } from "node:http";
 import { fileURLToPath } from "node:url";
 import { dirname, join } from "node:path";
+import { Server } from "socket.io";
 
 const app = express();
 const server = createServer(app);
+const io = new Server(server);
 
 const _dirname = dirname(fileURLToPath(import.meta.url));
 
 app.get("/", async (req, res) => {
   res.sendFile(join(_dirname, "index.html"));
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected!");
+  socket.on("chat message", (msg) => {
+    console.log("msg-- :", msg);
+  });
+  socket.on("disconnect", () => {
+    console.log("a user disconnected!");
+  });
 });
 
 const main = async () => {
